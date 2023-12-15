68610d8427ecf7b4a2f8a5bca8b2f37f623d68d1
YASIN 1881 <yasinnasiry18@gmail.com>
2023-12-15 12:57:01 +0330

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+module.exports = {
+  content: ["./project/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
