6743265f0275168a0a231b91a33ed6b1acddd59c
YASIN 1881 <yasinnasiry18@gmail.com>
2023-12-16 17:25:55 +0330

    inti font & color for project

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,30 @@ const plugin = require("tailwindcss/plugin");
 module.exports = {
   content: ["./project/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Poppins: "Poppins",/* 500 */
+        InterRegular: "Inter Regular",/* 400 */
+        InterSemiBold: "Inter SemiBold",/* 600 */ 
+        InterBold: "Inter Bold",/* 700 */
+        SpaceGroteskMedium: "SpaceGrotesk Medium", /* 500 */
+
+      },
+      colors: {
+        blue: "#377DFF",
+        green: "#38CB89",
+        orange: "#FFAB00",
+        red: "#FF5630",
+        primary1: "#000000",
+        neutral01: "#FEFEFE",
+        neutral02: "#F3F5F7",
+        neutral03: "#E8ECEF",
+        neutral04: "#6C7275",
+        neutral05: "#343839",
+        neutral06: "#232627",
+        neutral07: "#141718",
+      }
+    },
   },
   plugins: [],
 }
